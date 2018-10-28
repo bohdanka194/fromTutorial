c2aabc381c81f0bac236799c1c025180431d7282
Bohdana Arterchuk <bohdanka194@gmail.com>
2018-10-28 22:20:06 +0200

    add script to change img on click

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,24 @@
 
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+    console.log( "ready!" );
+
+    var myHeading = document.querySelector('h1');
+    myHeading.textContent = 'Hello world!';
+
+    // your code 
+    document.querySelector('.click-on-me').onclick = function() {
+        alert('Ouch! Stop poking me!');
+    }
+
+    document.querySelector('.click-on-me').textContent ="ahahahah";
+
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png'){
+        myImage.setAttribute('src', 'images/js770.jpg');
+    } else{
+        myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
